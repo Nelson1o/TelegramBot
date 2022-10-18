605c5d1b8d20abf69433279db7130ba74db614f0
Anton <anton.krivozubov@mail.ru>
2022-10-18 13:29:25 +0300

    ref code

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const token = '5677756025:AAHL7TeUpB6qMJqth7bN6gKQ7FRIA8HsvBE'
 
 const bot = new TelegramApi(token, {polling: true})
 
-bot.on("message", msg => {
-    const text = msg.text;
-    const chatId = msg.chat.id;
-    const userName = msg.from.first_name;
+const start = () => {
+    bot.on("message", async msg => {
+        const text = msg.text;
+        const chatId = msg.chat.id;
+        const userName = msg.from.first_name;
+    
+        if(text === '/start'){
+            await bot.sendMessage(chatId, "Бот запущен и готов к работе!");
+        }
+    
+        await bot.sendMessage(chatId, `Твое сообщение ${text}`);
+        console.log(`${userName} message: ${text}`);
+    })
+}
 
-    if(text === '/start'){
-        bot.sendMessage(chatId, "Бот запущен и готов к работе!");
-    }
-
-    bot.sendMessage(chatId, `Твое сообщение ${text}`);
-    console.log(`${userName} message: ${text}`);
-})
+start();
