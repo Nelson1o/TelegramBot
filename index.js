b9921cd9c0a51ff689cd155a8cf8f3fbccc4e0ed
Anton <anton.krivozubov@mail.ru>
2022-10-19 15:06:17 +0300

    new version

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
-const TelegramApi = require('node-telegram-bot-api')
+require('dotenv').config()
+const axios = require('axios');
+const express = require('express')
 
-const token = '5677756025:AAHL7TeUpB6qMJqth7bN6gKQ7FRIA8HsvBE'
+const TELEGRAM_URL = `http://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/getUpdates`;
+const TELEGRAM_URL_SEND = `http://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`;
 
-const bot = new TelegramApi(token, {polling: true})
-
-const start = () => {
-    bot.on("message", async msg => {
-        const text = msg.text;
-        const chatId = msg.chat.id;
-        const userName = msg.from.first_name;
-    
-        if(text === '/start'){
-            await bot.sendMessage(chatId, "Бот запущен и готов к работе!");
-        }
-    
-        await bot.sendMessage(chatId, `Твое сообщение ${text}`);
-        console.log(`${userName} message: ${text}`);
+const app = express();
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true
     })
+)
+
+const sendMessage = async (id) => {
+    await axios.post(TELEGRAM_URL_SEND + `?chat_id=${id}&text=Бот запущен и готов к работе!`)
 }
 
-start();
+let localId = 0;
+
+const condition = (id) => {
+    if(localId !== id){
+        localId = id;
+        return true
+    }
+    return false
+}
+
+const start = async () => {
+    setInterval(() => {
+        axios.get(TELEGRAM_URL)
+            .then(response => {
+                let mas = response.data.result;
+                let messageText = mas[mas.length - 1].message.text
+                let id = mas[mas.length - 1].message.message_id
+                if(condition(id)){
+                    console.log(messageText);
+                }
+                else{
+                    return
+                }                
+                if (messageText === "/start") {
+                    let chatId = mas[mas.length - 1].message.chat.id
+                    sendMessage(chatId);
+                }
+            })
+    }, 1000)
+}
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, async () => {
+    console.log(`Server running on port ${PORT}`);
+    await start();
+})
