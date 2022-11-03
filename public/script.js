821fdfa44adaec29289db52238b2cfef69540bc2
Anton <anton.krivozubov@mail.ru>
2022-11-03 20:29:41 +0300

    eslint

--- public/script.js
+++ public/script.js
@@ -1,56 +1,53 @@
 let timerId;
 let flag = false;
 let localId = 0;
-let messageContainer = document.querySelector(".message_container")
+const messageContainer = document.querySelector('.message_container');
 
 function CreateCard(obj) {
-    let date = new Date(obj.time);
+    const date = new Date(obj.time);
 
-    let card = document.createElement("div");
+    const card = document.createElement('div');
     card.innerHTML = `
     <div class="message_card">
         <div class="user_name">${obj.name}</div>
         <div class="message_text">${obj.text}</div>
         <div class="message_date">${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}</div>
     </div>
-    `
-    messageContainer.append(card)
+    `;
+    messageContainer.append(card);
 }
 
 const Condition = (id) => {
     if (localId !== id) {
         localId = id;
-        return true
+        return true;
     }
-    return false
-}
+    return false;
+};
 
 async function GetMessage() {
-    const response = await fetch("/api/getMessage", {
+    const response = await fetch('/api/getMessage', {
         method: 'GET',
-        headers: { 'Accept': 'application/json' }
+        headers: { Accept: 'application/json' },
     });
     if (response.ok === true) {
-        let obj = await response.json();
+        const obj = await response.json();
         if (Condition(obj.id)) {
             CreateCard(obj);
         }
-        else {
-            return;
-        }   
     }
     else {
         flag = true;
     }
 }
 
+const clearTimer = () => {
+    clearInterval(timerId);
+};
+
 timerId = setInterval(() => {
-    if(flag){
+    if (flag) {
         clearTimer();
     }
     GetMessage();
-}, 1000)
-
-const clearTimer = () => {
-    clearInterval(timerId);
-}
+}, 1000);
